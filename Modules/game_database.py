93aff7b1ae29079e029f96289f708efbeb0293fa
Bessmack <jenwied4lif@gmail.com>
2025-08-26 17:15:08 +0300

    Add two functions: save_score() and get_highest_score() which when called execute the supposed actions to the connected Database

--- Modules/game_database.py
+++ Modules/game_database.py
@@ -16,6 +16,27 @@ def init_db():
     db_connection.close()
 
 
+def save_score(player, score):
+    db_connection = sqlite3.connect(DATABASE)
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("""
+        INSERT INTO scores(player, score) VALUES (?, ?)
+    """,(player, int(score))
+    )
+    db_connection.commit()
+    db_connection.close()
 
 
+def get_highest_score(limit = 10):
+    db_connection = sqlite3.connect(DATABASE)
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(
+        """
+            SELECT player, score, created_at FROM scores ORDER BY score DESC, created_at ASC LIMIT ?
+        """, (limit)
+    )
+    rows = db_cursor.fetchall()
+    db_connection.commit()
+    db_connection.close()
 
+    return rows
